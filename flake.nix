811559aad94aa87a0ccc6dfddb5b7462bcce26b7
NixOS on Orange Pi 5 <no-email@no-email.com>
2024-04-07 20:26:00 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    unstable.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixos-rk3588.url = "github:ryan4yin/nixos-rk3588?rev=349f39dcaafeb41250544bcc066db8668a7762ce";
+  };
+
+  outputs = { nixpkgs, nixos-rk3588, ... }: let
+    # using the same nixpkgs as nixos-rk3588 to utilize the cross-compilation cache.
+    inherit (nixos-rk3588.inputs) nixpkgs;
+    system = "aarch64-linux";
+  in {
+    nixosConfigurations = {
+      opi5 = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {
+          rk3588 = { 
+            inherit nixpkgs; 
+            pkgsKernel = import nixpkgs { 
+              inherit system;
+            }; 
+          };
+        };
+
+        modules = [
+          nixos-rk3588.nixosModules.orangepi5.core
+#          nixos-rk3588.nixosModules.orangepi5.sd-image
+          ./system/configuration.nix
+          ./system/hardware-configuration.nix
+        ];
+      };
+    };
+  };
+}
