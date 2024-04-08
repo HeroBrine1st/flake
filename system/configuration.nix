fcea78c8c3725e655c632dfa1b6fa89d12182295
NixOS on Orange Pi 5 <no-email@no-email.com>
2024-04-08 21:17:01 +0300

    Add smartd notifications

--- system/configuration.nix
+++ system/configuration.nix
@@ -4,7 +4,54 @@
 
 { config, lib, pkgs, ... }:
 
+let smartdnotify = pkgs.writeShellApplication {
+    runtimeInputs = [ pkgs.curl pkgs.jq pkgs.coreutils ];
+    name = "smartdnotify";
+    text = ''
+#!/bin/bash
+
+set -e
+
+body=$(cat <<-END
 {
+  "content": $(jq --null-input --arg text "$SMARTD_MESSAGE" '$text'),
+  "embeds": [
+    {
+      "title": "$SMARTD_FAILTYPE",
+      "description": $(jq --null-input --arg text "$SMARTD_FULLMESSAGE" '$text'),
+      "color": 16711680,
+      "fields": [
+        {
+          "name": "Device",
+          "value": "$SMARTD_DEVICE ($SMARTD_DEVICETYPE)"
+        },
+        {
+          "name": "First encounter",
+          "value": "<t:$SMARTD_TFIRSTEPOCH:d>"
+        },
+        {
+          "name": "Message number",
+          "value": "$((SMARTD_PREVCNT + 1))",
+          "inline": true
+        },
+        {
+          "name": "Next message in",
+          "value": "$SMARTD_PREVCNT days",
+          "inline": true
+        }
+      ]
+    }
+  ],
+  "username": "Fusion",
+  "avatar_url": "https://cdn.discordapp.com/avatars/564403404688850973/5255509ed9be331c5b37aa873c279e6b.png",
+  "attachments": []
+}
+END
+)
+
+curl --header "Content-Type: application/json"  --request POST --data "$body" --silent --show-error https://discord.com/api/webhooks/658390924052660246/1nSDsGIsGvyT3EsMKoaWCyAcGyZxQ0T37nNBN29chel-4EtWgWhgpY1c8gfTqRqsNsfV
+    '';
+  }; in {
   boot.tmp.useTmpfs = true;
 
   nix = {
@@ -70,6 +117,7 @@
     psmisc
     duplicity
     ctop
+    smartdnotify
   ];
 
   programs.nano.nanorc = ''
@@ -90,7 +138,22 @@
 #    '';
   };
   services.udisks2.enable = true;
-  services.smartd.enable = true;
+
+  services.smartd = {
+    enable = true;
+    devices = [
+      {
+        device = "/dev/disk/by-id/ata-ST1000LM035-1RK172_WQ9C5SMW";
+        options = "-H -p -f -t -l error -l selftest -l selfteststs -C 197 -U 198 -m noemail@example.com -M test -M exec ${smartdnotify}/bin/smartdnotify";
+      }
+      {
+        device = "/dev/nvme0";
+        options = "-d nvme -H -l error -W 5,60,80 -m noemail@example.com -M test -M exec ${smartdnotify}/bin/smartdnotify";
+      }
+    ];
+    notifications.test = true;
+    notifications.wall.enable = false;
+  };
 
   services.cron = {
     enable = true;
