832ba020290c7b66ae93c6f2022859863dbcc45e
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-04-30 22:50:03 +0300

    Fix solidexplorer ssh availability

--- system/opi5/configuration.nix
+++ system/opi5/configuration.nix
@@ -93,6 +93,12 @@ in
     settings = {
       PermitRootLogin = "without-password";
       PasswordAuthentication = false;
+      Macs = [
+        "hmac-sha2-512"
+        "hmac-sha2-512-etm@openssh.com"
+        "hmac-sha2-256-etm@openssh.com"
+        "umac-128-etm@openssh.com"
+      ];
     };
 # doesn't work
 #    extraConfig = ''
--- system/pc/configuration.nix
+++ system/pc/configuration.nix
@@ -294,6 +294,12 @@
     settings = {
       PermitRootLogin = "without-password";
       PasswordAuthentication = false;
+      Macs = [
+        "hmac-sha2-512"
+        "hmac-sha2-512-etm@openssh.com"
+        "hmac-sha2-256-etm@openssh.com"
+        "umac-128-etm@openssh.com"
+      ];
     };
   };
 
