e990862c6222b6625bcfd4e7b35b5251090c05f5
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-05-14 01:25:31 +0300

    Use newer firmware

--- packages/mali-firmware.nix
+++ packages/mali-firmware.nix
@@ -3,11 +3,11 @@
   fetchurl,
 }: stdenv.mkDerivation {
   pname = "mali-g610-firmware";
-  version = "unstable-2022-11-21";
+  version = "unstable-2023-06-08";
 
   src = fetchurl {
-    url = "https://github.com/JeffyCN/rockchip_mirrors/raw/309268f7a34ca0bba0ab94a0b09feb0191c77fb8/firmware/g610/mali_csffw.bin";
-    hash = "sha256-rZFRGnf6EQr0aiopytniJVJNnPqf41vc6WYG01TmB94=";
+    url = "https://github.com/tsukumijima/libmali-rockchip/raw/2d267b0ab97b9116a75f8b606204297a200db37c/firmware/g610/mali_csffw.bin";
+    hash = "";
   };
 
   buildCommand = ''
--- system/opi5/hardware-configuration.nix
+++ system/opi5/hardware-configuration.nix
@@ -91,7 +91,7 @@ in {
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 
-  hardware.firmware = [
+  hardware.firmware = lib.mkOverride -1 [
     mali-firmware
   ];
 
