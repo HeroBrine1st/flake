26abef47809da7cc1f19b0b093f956b84e305f26
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-05-15 17:06:31 +0300

    OPi5: Update to https://github.com/ryan4yin/nixos-rk3588/pull/35

--- flake.nix
+++ flake.nix
@@ -5,8 +5,7 @@
     pkgs-unstable.url = "nixpkgs/nixos-unstable";
     pkgs-stable.url = "nixpkgs/nixos-23.11";
     pkgs-jetbrains-2022-3-3.url = "github:NixOS/nixpkgs?rev=f6aa4144d0c38231d8c383facf40f63b13759bb5";
-    #nixos-rk3588.url = "github:ryan4yin/nixos-rk3588?rev=349f39dcaafeb41250544bcc066db8668a7762ce";
-    nixos-rk3588.url = "github:HeroBrine1st/nixos-rk3588?rev=7f20d6763f6e8a19b0ce361efce5c0bab776001d";
+    nixos-rk3588.url = "github:ryan4yin/nixos-rk3588?rev=c4fef04d8c124146e6e99138283e0c57b2ad8e29";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs = {
