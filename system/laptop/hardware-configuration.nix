5a892d9a130a325130d2f9b09411935caee2c06d
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-05-28 23:01:39 +0300

    Remove keyfile from config

--- system/laptop/hardware-configuration.nix
+++ system/laptop/hardware-configuration.nix
@@ -25,7 +25,6 @@
                 name = "root";
                 settings = {
                   allowDiscards = true;
-                  keyFile = "/tmp/root.key";
                 };
                 content = {
                   type = "btrfs";
