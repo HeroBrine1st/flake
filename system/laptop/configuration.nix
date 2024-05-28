f2efaef0695c1edfcc5f3b384918b55722588366
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-05-28 21:53:28 +0300

    Temporarily enable ssh

--- system/laptop/configuration.nix
+++ system/laptop/configuration.nix
@@ -1,3 +1,7 @@
 {
-  services.openssh.enable = false;
+  services.openssh.enable = true;
+
+  users.users.herobrine1st.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPRZ4DbHhpQDVC8H578LRrHKO4DgaPoPNs3Icdkiv8XE herobrine1st@DESKTOP-IJK2GUG"
+  ];
 }
