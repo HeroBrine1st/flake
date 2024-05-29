2c7396d283c540e7ebe825bb0352c42b3b9760a8
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-05-29 10:04:46 +0300

    Laptop: disable ssh

--- system/laptop/configuration.nix
+++ system/laptop/configuration.nix
@@ -1,7 +1,3 @@
 {
-  services.openssh.enable = true;
-
-  users.users.herobrine1st.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPRZ4DbHhpQDVC8H578LRrHKO4DgaPoPNs3Icdkiv8XE herobrine1st@DESKTOP-IJK2GUG"
-  ];
+  services.openssh.enable = false;
 }
