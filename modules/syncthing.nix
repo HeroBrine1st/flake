ffc074661511ee0e447955b594c8aaa04543b89b
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-06-02 16:19:30 +0300

    Add devices to syncthing

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -10,26 +10,28 @@
     "DESKTOP-IJK2GUG" = {
       id = "GLK2ETE-TUHZ43A-GAIRJYY-THAAOFO-GIAX7L5-DJO5NCF-NUN7GAR-DDGTZAM";
     };
+    "lynx" = {
+      id = "3HCNB6K-LJN74OU-TVIZNB4-RBWZA7T-JRPIMTW-6ZK5BRS-2WJEB5V-5AEVGQD";
+    };
   } [ config.networking.hostName ];
-  devices = lib.lists.remove config.networking.hostName [ "OPi5" "MOBILE-DCV5AQD" "DESKTOP-IJK2GUG" ];
 in {
   services.syncthing = {
     user = "herobrine1st";
     configDir = "/home/herobrine1st/.config/Syncthing";
     overrideDevices = true;
-    overrideFolders = true;
+    overrideFolders = false;
     settings = {
       devices = deviceIds;
       folders = {
         "uf77h-ptigu" = {
           label = "Secure";
           path = "/mnt/secure";
-          devices = devices;
+          devices = lib.lists.remove config.networking.hostName [ "OPi5" "MOBILE-DCV5AQD" "DESKTOP-IJK2GUG" ];
         };
         "yb6rg-qs9gm" = {
           label = "Local Music";
           path = "/home/herobrine1st/Music/Main";
-          devices = devices;
+          devices = lib.lists.remove config.networking.hostName [ "OPi5" "MOBILE-DCV5AQD" "DESKTOP-IJK2GUG" ];
         };
       };
     };
