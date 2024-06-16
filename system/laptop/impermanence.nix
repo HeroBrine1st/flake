f1e60166e907b5ba0c0854a40e6d3d59e13b354f
herobrine1st <pankov.76.tvink@gmail.com>
2024-06-17 01:44:14 +0300

    laptop: update impermanence

--- system/laptop/impermanence.nix
+++ system/laptop/impermanence.nix
@@ -8,7 +8,9 @@
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
+      "/var/lib/docker"
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+      { directory = "/mnt/secure"; user = "herobrine1st"; group = "users"; }
     ];
     files = [
       "/etc/machine-id"
