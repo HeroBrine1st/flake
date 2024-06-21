b20940d5bc88482afea33fa03d6868535a11b88e
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-06-21 12:24:29 +0300

    Update impermanence.nix

--- system/laptop/impermanence.nix
+++ system/laptop/impermanence.nix
@@ -9,6 +9,7 @@
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
       "/var/lib/docker"
+      "/etc/secureboot"
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
       { directory = "/mnt/secure"; user = "herobrine1st"; group = "users"; }
     ];
