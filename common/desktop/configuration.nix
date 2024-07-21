63fac7f6efba0b5cc224ea6cd2197747feadb84a
herobrine1st <pankov.76.tvink@gmail.com>
2024-07-21 18:15:51 +0300

    Remove bash-completion override

--- common/desktop/configuration.nix
+++ common/desktop/configuration.nix
@@ -39,15 +39,6 @@
 #    ];
   };
 
-  # https://github.com/scop/bash-completion/issues/1157
-  programs.bash.completion.package = pkgs.bash-completion.overrideAttrs (old: rec {
-    version = "2.11";
-    src = pkgs.fetchurl {
-      url = "https://github.com/scop/${old.pname}/releases/download/${version}/${old.pname}-${version}.tar.xz";
-      sha256 = "sha256-c6iJS62U3ug6tGj6CfYo2v/VZ+i+8aJCd/HpoNr5Eaw=";
-    };
-  });
-
   environment.systemPackages = with pkgs; [
     mangohud
     gamescope
