31b9decc433b37d52c3feed8030c26423f7a588a
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-09-22 19:36:23 +0300

    opi5: remove old drive

--- system/opi5/configuration.nix
+++ system/opi5/configuration.nix
@@ -71,10 +71,10 @@ in
         device = "/dev/disk/by-id/ata-ST1000LM035-1RK172_WQ9C5SMW";
         options = "-H -p -f -t -l error -l selftest -l selfteststs -C 197 -U 198 -m noemail@example.com -M test -M exec ${smartdnotify}/bin/smartdnotify";
       }
-      {
-        device = "/dev/disk/by-id/ata-WDC_WD10EALX-009BA0_WD-WCATR9846787";
-        options = "-H -p -f -t -l error -l selftest -l selfteststs -C 197 -U 198 -m noemail@example.com -M test -M exec ${smartdnotify}/bin/smartdnotify";
-      }
+#      {
+#        device = "/dev/disk/by-id/ata-WDC_WD10EALX-009BA0_WD-WCATR9846787";
+#        options = "-H -p -f -t -l error -l selftest -l selfteststs -C 197 -U 198 -m noemail@example.com -M test -M exec ${smartdnotify}/bin/smartdnotify";
+#      }
       {
         device = "/dev/nvme0";
         options = "-d nvme -H -l error -W 5,60,80 -m noemail@example.com -M test -M exec ${smartdnotify}/bin/smartdnotify";
