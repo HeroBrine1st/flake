417852261da72aacfb2cc451ef931fb3d340b6e7
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-10-11 21:19:49 +0300

    opi5: update btrbk

--- system/opi5/btrbk.nix
+++ system/opi5/btrbk.nix
@@ -2,20 +2,26 @@
   services.btrbk = {
     instances = {
       "docker_data" = {
-        #onCalendar = "daily";
-        onCalendar = null;
+        onCalendar = "hourly";
         settings = {
           # stream_compress = "zstd";
 
           volume."/.fsroot" = { # base directory
-            target = "/mnt/basic/.fsroot/docker_data_snapshots/"; # btrfs send-receive target
-            snapshot_dir = "@docker_data/snapshots/";
-            snapshot_preserve = "12h 7d 4w 12m 2y";
-            snapshot_preserve_min = "2d";
-            target_preserve = "12h 7d 4w 12m 2y";
-            target_preserve_min = "2d";
             subvolume."@docker_data/*" = {
-
+              target = "/mnt/basic/.fsroot/docker_data_snapshots/"; # btrfs send-receive target
+              snapshot_dir = "@docker_data/snapshots/";
+              snapshot_preserve = "24h 7d 4w";
+              snapshot_preserve_min = "2d";
+              target_preserve = "24h 7d 4w 12m 2y";
+              target_preserve_min = "2d";
+            };
+            subvolume."@docker_definitions" = {
+              target = "/mnt/basic/.fsroot/docker_definitions_snapshots/"; # btrfs send-receive target
+              snapshot_dir = "snapshots/";
+              snapshot_preserve = "24h 7d 4w";
+              snapshot_preserve_min = "14d";
+              target_preserve = "24h 7d 4w 12m 2y";
+              target_preserve_min = "14d";
             };
           };
         };
