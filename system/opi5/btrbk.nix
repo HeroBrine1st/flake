6b5b6972f1cec8a184c99d292d27a501bb0f7b4f
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-10-11 20:25:24 +0300

    opi5: update btrbk

--- system/opi5/btrbk.nix
+++ system/opi5/btrbk.nix
@@ -11,7 +11,9 @@
             target = "/mnt/basic/.fsroot/docker_data_snapshots/"; # btrfs send-receive target
             snapshot_dir = "@docker_data/snapshots/";
             snapshot_preserve = "12h 7d 4w 12m 2y";
-            snapshot_preserve_min = "7d";
+            snapshot_preserve_min = "2d";
+            target_preserve = "12h 7d 4w 12m 2y";
+            target_preserve_min = "2d";
             subvolume."@docker_data/*" = {
 
             };
