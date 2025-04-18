1731a73cf8acde08a743e7d14be987ac1ee8c8bf
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-04-18 12:45:14 +0300

    pkgs-unstable: move to small channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    pkgs-unstable.url = "nixpkgs/nixos-unstable";
+    pkgs-unstable.url = "nixpkgs/nixos-unstable-small";
     pkgs-stable.url = "nixpkgs/nixos-24.11";
     pkgs-jetbrains-2022.url = "github:NixOS/nixpkgs?rev=e1fa54a56982c5874f6941703c8b760541e40db1";
     pkgs-bdfr.url = "github:NixOS/nixpkgs?rev=59b1aef59071cae6e87859dc65de973d2cc595c0"; # pinned
