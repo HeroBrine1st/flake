c132d663111675471352bccb46ec6d5a269f9a82
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-04-22 19:10:32 +0300

    bravo: set network.overlay.configAppendixFile

--- system/bravo/impermanence.nix
+++ system/bravo/impermanence.nix
@@ -48,4 +48,6 @@
   users.users.herobrine1st.createHome = true;
   users.users.root.hashedPasswordFile = "/nix/persist/password/root"; # REQUIRED ON INSTALLATION
   users.mutableUsers = false;
+
+  network.overlay.configAppendixFile = "/nix/persist/nebula/local.yaml";
 }
