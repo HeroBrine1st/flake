1d050c48048ea5a399c71cf1f746507ca1357ba0
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-04-22 19:46:41 +0300

    bravo: remove extra definition

--- system/bravo/default.nix
+++ system/bravo/default.nix
@@ -6,13 +6,4 @@
     (modulesPath + "/profiles/qemu-guest.nix")
     ./traefik.nix
   ];
-
-  services.traefik = {
-    # TODO move traefik to module and move that out of here
-    docker = {
-      enable = true;
-      ports = [ "[::]:443:443" ];
-    };
-    group = "docker";
-  };
 }
