3914e28e869cb679e3614d4e834c70dd8bd6f1a3
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-05-04 13:41:06 +0300

    Copy auto-generated features list from error message

--- system/foxtrot/hydra.nix
+++ system/foxtrot/hydra.nix
@@ -10,7 +10,8 @@
     {
       hostName = "localhost";
       protocol = null;
-      systems = [ "x86_64-linux" "aarch64-linux" ] ;
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      # I am a 'x86_64-linux' with features {benchmark, big-parallel, kvm, nixos-test}
       supportedFeatures = [ "kvm" "nixos-test" "big-parallel" "benchmark" ];
     }
   ];
