6d00069b509ec78ecec9983d2bdbf50008e5997d
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-05-04 20:25:21 +0300

    Use private binary cache on all systems in overlay network

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./nix
     ./cli
     ./network/overlay
     #./scrutiny-collector.nix TODO enable only on bare metal machines
---
+++ modules/common/nix.nix
@@ -0,0 +1,10 @@
+{ lib, config, ... }: {
+  nix.settings = lib.mkIf network.overlay.enabled {
+    substituters = [
+      "http://10.168.88.10:5000"
+    ];
+    trusted-public-keys = [
+      "foxtrot-cache:K5TJ723kF9QLxanfPFj0tHCHMEtKwnKkY56cAtOTg6Y="
+    ];
+  };
+}
