516bf5a0628bbcd75ae1f734c22ac1ccc6cae326
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-05-04 13:39:23 +0300

    amend! foxtrot: enable aarch64-linux for hydra

--- system/foxtrot/hydra.nix
+++ system/foxtrot/hydra.nix
@@ -19,7 +19,6 @@
     enable = true;
     hydraURL = "http://10.168.88.10:3000";
     notificationSender = "hydra@localhost";
-    buildMachinesFiles = [];
     useSubstitutes = true;
     listenHost = "10.168.88.10";
   };
