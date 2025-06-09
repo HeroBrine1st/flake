03b60f68ce49ad81ce9baecd833984df924530a7
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-06-09 16:05:26 +0300

    Use systems.nix for syncthing module

--- modules/common/syncthing.nix
+++ modules/common/syncthing.nix
@@ -1,24 +1,22 @@
-{ config, lib, ... }: let
-  deviceIds = {
-    "alfa".id = "QDEHB5B-UGM2GQ6-3VG3RBL-JXOTGB2-OGNO5AL-H7KAFIC-MESTOWJ-O2YD3AO";
-    "MOBILE-DCV5AQD".id = "HLDBCKF-TWJF3WC-4VOPZPK-BHI4I3M-66HMB4M-3IJCFCB-ZBQVV4U-VEFL3AN";
-    "DESKTOP-IJK2GUG".id = "GLK2ETE-TUHZ43A-GAIRJYY-THAAOFO-GIAX7L5-DJO5NCF-NUN7GAR-DDGTZAM";
-    "lynx".id = "3HCNB6K-LJN74OU-TVIZNB4-RBWZA7T-JRPIMTW-6ZK5BRS-2WJEB5V-5AEVGQD";
-  };
+{ config, lib, systems, ... }: let
   hostname = config.networking.hostName;
+  cfg = config.services.syncthing;
+  currentSystem = systems."${hostname}";
   mkFolder = folder: folder // {
     enable = builtins.elem hostname folder.devices; # Disable if restricted
     devices = lib.lists.remove hostname folder.devices; # Remove this machine from config
+    # Place all folders in one directory if it is a server
+    path = if currentSystem.isServer then "${cfg.dataDir}/${builtins.baseNameOf folder.path}" else folder.path;
   };
 in {
-  services.syncthing = lib.mkIf (builtins.elem hostname (builtins.attrNames deviceIds)) {
+  services.syncthing = lib.mkIf (currentSystem ? syncthing) {
     enable = true;
     user = "herobrine1st";
-    configDir = "/home/herobrine1st/.config/Syncthing";
+    configDir = lib.mkDefault "/home/herobrine1st/.config/Syncthing";
     overrideDevices = true;
-    overrideFolders = false;
+    overrideFolders = lib.mkDefault false;
     settings = {
-      devices = builtins.removeAttrs deviceIds [ hostname ]; # Remove this machine from config
+      devices = builtins.mapAttrs (k: v: v.syncthing) (builtins.removeAttrs (lib.attrsets.filterAttrs (k: v: v ? syncthing) systems) [hostname]);
       folders = {
         "uf77h-ptigu" = mkFolder {
           label = "Secure";
@@ -46,10 +44,10 @@ in {
 
   networking.firewall = lib.mkIf config.services.syncthing.enable {
     allowedTCPPorts = [
-      22000 # syncthing
+      22000
     ];
     allowedUDPPorts = [
-      22000 21027 # syncthing
+      22000 21027
     ];
   };
 }
--- systems.nix
+++ systems.nix
@@ -4,6 +4,7 @@
     networks.overlay.address = "10.168.88.72";
     isServer = true;
     isStatic = true;
+    syncthing.id = "QDEHB5B-UGM2GQ6-3VG3RBL-JXOTGB2-OGNO5AL-H7KAFIC-MESTOWJ-O2YD3AO";
   };
   "bravo" = {
     networks.overlay.address = "10.168.88.57";
@@ -19,11 +20,13 @@
     networks.overlay.address = "10.168.88.254";
     isServer = false;
     isStatic = false;
+    syncthing.id = "GLK2ETE-TUHZ43A-GAIRJYY-THAAOFO-GIAX7L5-DJO5NCF-NUN7GAR-DDGTZAM";
   };
   "MOBILE-DCV5AQD" = {
     networks.overlay.address = "10.168.88.153";
     isServer = false;
     isStatic = false;
+    syncthing.id = "HLDBCKF-TWJF3WC-4VOPZPK-BHI4I3M-66HMB4M-3IJCFCB-ZBQVV4U-VEFL3AN";
   };
   "foxtrot" = {
     networks.overlay.address = "10.168.88.10";
@@ -34,5 +37,6 @@
     networks.overlay.address = "10.168.88.153";
     isServer = false;
     isStatic = false;
+    syncthing.id = "3HCNB6K-LJN74OU-TVIZNB4-RBWZA7T-JRPIMTW-6ZK5BRS-2WJEB5V-5AEVGQD";
   };
 }
