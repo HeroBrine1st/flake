b39f0b22f6aee3f6a1cd209e3b2cc0f84132e44d
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-07-06 14:18:10 +0300

    nix flake update except ags due to broken ags

--- flake.nix
+++ flake.nix
@@ -25,7 +25,14 @@
       inputs.nixpkgs.follows = "pkgs-unstable";
     };
     ags = {
-      url = "github:aylur/ags";
+      # ags is broken on at least e5e2efaf2e05e077190cfddabf5b827edae5ef8b
+      # ags bundle requires astal/gtk3/jsx-runtime on gtk4 projects
+      # no packages including astal3, astal4 and agsFull provide jsx-runtime
+      # jsx-runtime is found on npm https://www.npmjs.com/package/jsx-runtime
+      # however it requires exactly astal/gtk3/jsx-runtime
+      # The only solution is to install another shell (aylurs-gtk-shell) using Arch Linux: https://github.com/Jas-SinghFSU/HyprPanel/issues/1033#issuecomment-3031248116
+      # I don't want neither Arch Linux nor another shell
+      url = "github:aylur/ags?rev=3ed9737bdbc8fc7a7c7ceef2165c9109f336bff6";
       inputs.nixpkgs.follows = "pkgs-unstable";
     };
   };
