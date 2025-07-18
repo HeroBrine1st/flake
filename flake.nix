58f284e37d36d5df8eb316e3805896d4ed4f4c6f
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-07-18 12:55:24 +0300

    nix flake update and move stable to small

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     pkgs-unstable.url = "nixpkgs/nixos-unstable-small";
-    pkgs-stable.url = "nixpkgs/nixos-25.05";
+    pkgs-stable.url = "nixpkgs/nixos-25.05-small";
     pkgs-jetbrains-2022.url = "github:NixOS/nixpkgs?rev=e1fa54a56982c5874f6941703c8b760541e40db1";
     pkgs-bdfr.url = "github:NixOS/nixpkgs?rev=59b1aef59071cae6e87859dc65de973d2cc595c0"; # pinned
     nixos-rk3588.url = "github:HeroBrine1st/nixos-rk3588?rev=0b8b677e400fe178e46e0f04175ac2b9b6441b82"; # pinned
