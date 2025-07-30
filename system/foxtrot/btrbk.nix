a1003b181a9452a9de00b7a2e1207176f7aec019
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-07-30 16:22:29 +0300

    foxtrot: snapshot additional subvolumes

--- system/foxtrot/btrbk.nix
+++ system/foxtrot/btrbk.nix
@@ -4,8 +4,8 @@
       "docker" = {
         onCalendar = "hourly";
         settings = {
+          snapshot_create = "onchange";
           volume."/.fsroot" = {
-            snapshot_create = "onchange";
             subvolume."@nix/persist/system/var/docker_data/*" = {
               snapshot_dir = "@archives/docker_data_ssd/";
               snapshot_preserve = "24h 7d 4w";
@@ -24,9 +24,26 @@
             };
           };
           volume."/mnt/brp" = {
-            snapshot_create = "onchange";
             subvolume."@docker_data/*" = {
               snapshot_dir = "@archives/docker_data_hdd/";
+              snapshot_preserve = "24h 7d 4w 12m 2y";
+              snapshot_preserve_min = "14d";
+            };
+          };
+        };
+      };
+      "general" = {
+        onCalendar = "hourly";
+        settings = {
+          snapshot_create = "onchange";
+          volume."/mnt/brp" = {
+            subvolume."@user" = {
+              snapshot_dir = "@archives/user/";
+              snapshot_preserve = "24h 7d 4w 12m 2y";
+              snapshot_preserve_min = "14d";
+            };
+            subvolume."@media" = {
+              snapshot_dir = "@archives/media/";
               snapshot_preserve = "24h 7d 4w 12m 2y";
               snapshot_preserve_min = "14d";
             };
