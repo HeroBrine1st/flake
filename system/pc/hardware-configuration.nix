6cfd16cc15336d96bf7025434a05895db334dd18
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-08-08 19:37:54 +0300

    Fix firejail profile

--- system/pc/hardware-configuration.nix
+++ system/pc/hardware-configuration.nix
@@ -180,8 +180,8 @@
       env LIBVA_DRIVER_NAME=nvidia
       env MOZ_X11_EGL=1
       env __EGL_VENDOR_LIBRARY_FILENAMES=${config.hardware.nvidia.package}/share/glvnd/egl_vendor.d/10_nvidia.json
-      NVD_BACKEND=direct
-      MOZ_DISABLE_RDD_SANDBOX=1
+      env NVD_BACKEND=direct
+      env MOZ_DISABLE_RDD_SANDBOX=1
     '';
   };
 }
