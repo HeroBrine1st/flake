351e0df908ad91967979a36e175baef187d2aa56
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-08-09 13:14:24 +0300

    Rotate binary cache signing key

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -8,7 +8,7 @@
         "http://10.168.88.10:5000"
       ];
       trusted-public-keys = [
-        "foxtrot-cache:K5TJ723kF9QLxanfPFj0tHCHMEtKwnKkY56cAtOTg6Y="
+        "foxtrot-cache:D4KUj8rMcBw9zGnsQndxhVztVuWfJhGy1fGI3l60ztE="
       ];
     })
   ];
