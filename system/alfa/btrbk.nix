adc29d8380d0a47537be6dd23f3a3e7140d63c8f
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-08-30 16:56:13 +0300

    alfa: remove unconnected drives from btrbk

--- system/alfa/btrbk.nix
+++ system/alfa/btrbk.nix
@@ -25,27 +25,6 @@
               target_preserve_min = "14d";
             };
           };
-          volume."/mnt/arp" = {
-            snapshot_create = "onchange";
-            subvolume."@docker_data/*" = {
-              snapshot_dir = "@docker_data/snapshots/";
-              snapshot_preserve = "24h 7d 4w 12m 2y";
-              snapshot_preserve_min = "14d";
-            };
-          };
-        };
-      };
-      "basic" = {
-        onCalendar = "daily";
-        settings = {
-          volume."/mnt/arp" = {
-            snapshot_create = "onchange";
-            subvolume."@basic" = {
-              snapshot_dir = "snapshots/";
-              snapshot_preserve = "24h 7d 4w 12m 2y";
-              snapshot_preserve_min = "14d";
-            };
-          };
         };
       };
     };
