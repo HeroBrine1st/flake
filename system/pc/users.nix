c6641641f75f01177d844b9530e7fd18b830a1eb
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-08-31 21:57:02 +0300

    remove ssh key

--- system/pc/users.nix
+++ system/pc/users.nix
@@ -1,6 +1,5 @@
 {
   users.users.herobrine1st.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhbfIlo673pgcavZFZyKWoMf+Ak0ETfyD1Y89YJJnue solidexplorer@lynx"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILg745qCXMEfCllfeie+7DZM9K9hNA1b0uxXLQasQO2A herobrine1st@lynx"
   ];
 
