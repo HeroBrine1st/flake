216ed65e96c4dd4bbf43e4f3f50adc5887d0040a
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-08-31 17:48:37 +0300

    common/cli/zsh: fully replicate powerline theme

--- modules/common/cli/zsh/oh-my-zsh.nix
+++ modules/common/cli/zsh/oh-my-zsh.nix
@@ -2,12 +2,21 @@
   programs.zsh.ohMyZsh = {
     enable = true;
     customPkgs = [
-      # The zsh module copies ${customPkgs[@]/#/share/zsh} but powerlevel has wrong derivation layout
-      (pkgs.runCommandLocal "powerlevel10k-pluggable" {} ''
+      (let
+        powerlevel10k = pkgs.zsh-powerlevel10k.overrideAttrs(old: {
+          patches = old.patches ++ [
+            ./powerlevel10k-flexible-git.patch
+          ];
+        });
+        # The zsh module copies ${customPkgs[@]/#/share/zsh} but powerlevel has wrong derivation layout
+      in pkgs.runCommandLocal "powerlevel10k-pluggable" {} ''
         install -d $out/share/zsh/themes/
-        ln -s ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k $out/share/zsh/themes/powerlevel10k
+        ln -s ${powerlevel10k}/share/zsh-powerlevel10k $out/share/zsh/themes/powerlevel10k
       '')
     ];
+    preLoaded = ''
+      source ${./p10k.zsh}
+    '';
     theme = "powerlevel10k/powerlevel10k";
     plugins = [
       "git"
