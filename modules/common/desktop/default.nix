4401805050f36f2624e0e972edd8c2dc43228937
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-10-06 21:39:15 +0300

    common/desktop: use default ssh Macs set

--- modules/common/desktop/default.nix
+++ modules/common/desktop/default.nix
@@ -77,12 +77,6 @@
     settings = {
       PermitRootLogin = "no";
       PasswordAuthentication = false;
-      Macs = [
-        "hmac-sha2-512" # support some legacy clients
-        "hmac-sha2-512-etm@openssh.com"
-        "hmac-sha2-256-etm@openssh.com"
-        "umac-128-etm@openssh.com"
-      ];
     };
   };
 
