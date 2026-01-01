2979c527a6ae9432a7d103b9012f4fbf36f41f19
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2026-01-01 15:38:22 +0300

    common/desktop: enable flatpak

--- modules/common/desktop/default.nix
+++ modules/common/desktop/default.nix
@@ -108,6 +108,15 @@
     };
   };
 
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
   system.stateVersion = "23.11"; # Do not change
 }
 
