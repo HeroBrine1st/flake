8aca847f85c2878bf02ed2bb7a7d461e21513581
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2026-01-02 22:37:52 +0300

    update lanzaboote to v1.0.0

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     };
     impermanence.url = "github:nix-community/impermanence";
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.3";
+      url = "github:nix-community/lanzaboote/v1.0.0";
       inputs.nixpkgs.follows = "pkgs-unstable";
     };
     fenix = {
