87b81e7dfbed96ceef44658e53909c97fd1c5386
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2026-02-01 00:39:11 +0300

    Disable cache on machine hosting it

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -1,13 +1,15 @@
-{ lib, config, ... }: {
+{ lib, config, systems, ... }: {
   options.nix.useFlakeCache = (lib.mkEnableOption "usage of cache from build machine") // { default = true; };
 
   config.nix.settings = lib.mkMerge [
     {
       auto-optimise-store = true;
     }
-    (lib.mkIf (config.network.overlay.enabled && config.nix.useFlakeCache ){
+    (let
+      host = "10.168.88.10";
+    in lib.mkIf (config.network.overlay.enabled && config.nix.useFlakeCache && systems."${config.networking.hostName}".networks.overlay.address != host) {
       substituters = [
-        "http://10.168.88.10:5000"
+        "http://${host}:5000"
       ];
       trusted-public-keys = [
         "foxtrot-cache:X8r3Ux9DlsIqkM0uMIw6gAlwCUCW324gm2VZn7eMJr0="
