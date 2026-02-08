d84f0c2d5e2ab12eb96a672e51d646c28b4f2add
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2026-02-08 12:41:23 +0300

    testing: update kata-containers

--- system/testing/configuration.nix
+++ system/testing/configuration.nix
@@ -30,20 +30,11 @@
     extraPackages = [
       (pkgs.kata-runtime.overrideAttrs(old: {
         hardeningDisable = [];
-        # apply full patch not restricted by sourceRoot, as it is a complete patch ready for upstreaming
-        # the patch is taken from https://github.com/kata-containers/kata-containers/pull/7648 and updated to resolve conflicts
-        # I know about -p3 and --directory, no both won't work (in latter case /build/source/tests is read-only)
-        src = pkgs.stdenv.mkDerivation rec {
-          src = pkgs.fetchFromGitHub {
-            owner = "kata-containers";
-            repo = "kata-containers";
-            rev = "758471cbddeff44297ca5a3db9340bad0c3360ef"; # pull/11749/head
-            hash = "sha256-tH395Uz1vdm/9rvFVKcfycMEuSDfWJ9S798n0wb7EBg=";
-          };
-          name = src.name;
-
-          phases = [ "unpackPhase" "patchPhase" "installPhase" ];
-          installPhase = "cp --archive . $out";
+        src = pkgs.fetchFromGitHub {
+          owner = "kata-containers";
+          repo = "kata-containers";
+          rev = "4fe59cf0992375d6aca44dfe05f9192f98de5a50"; # pull/11749/head
+          hash = "sha256-tWHiNiebQgTEAkFx8Kt+ABlyJNnxK5r4/prvEeTW8JU=";
         };
       }))
     ];
